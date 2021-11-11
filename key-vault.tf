0ed461b48fc044865a262ce04fc3bbb44249ff52
abidfs <abid.sayyad@hmcts.net>
2021-11-11 13:36:12 +0000

    using correct product_group_name and commented other section

--- key-vault.tf
+++ key-vault.tf
@@ -6,16 +6,18 @@ module "adoption-app-vault" {
   tenant_id                  = var.tenant_id
   object_id                  = var.jenkins_AAD_objectId
   resource_group_name        = azurerm_resource_group.rg.name
-  product_group_name         = "dcd_group_adoption_v2"
+  product_group_name         = "DTS Adoption"
   common_tags                = local.tags
   create_managed_identity    = true
   // managed_identity_object_id = var.managed_identity_object_id
 }
 
-data "azurerm_key_vault" "adoption-app_key_vault" {
+/*
+data "azurerm_key_vault" "adoption_key_vault" {
   name                = "${var.product}-${var.env}" //test "adoption-app-${var.env}"
   resource_group_name = azurerm_resource_group.rg.name
 }
+*/
 
 output "vaultName" {
   value = module.adoption-app-vault.key_vault_name
