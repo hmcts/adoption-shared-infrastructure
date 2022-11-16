9b235a66dbf80e45ab54948d914ce147070fbc90
cshiwarale <98115959+cshiwarale@users.noreply.github.com>
2022-11-16 14:04:21 +0000

    Adding tag for AppInsight resource in preview environment

--- application-insights.tf
+++ application-insights.tf
@@ -20,3 +20,28 @@ resource "azurerm_key_vault_secret" "appInsights-InstrumentationKey" {
   value        = azurerm_application_insights.appinsights.instrumentation_key
   key_vault_id = module.adoption-app-vault.key_vault_id
 }
+
+resource "azurerm_application_insights" "appinsights_preview" {
+  name                = "${var.product}-appinsights-preview"
+  location            = var.appinsights_location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = var.application_type
+  count = var.env == "aat" ? 1 : 0
+
+  tags = var.common_tags
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
+      # destroys and re-creates this appinsights instance..
+      application_type,
+    ]
+  }
+}
+
+resource "azurerm_key_vault_secret" "AZURE_APPINSIGHTS_KEY_PREVIEW" {
+  name         = "AppInsightsInstrumentationKey-Preview"
+  value        = azurerm_application_insights.appinsights_preview[0].instrumentation_key
+  key_vault_id = module.adoption-app-vault.key_vault_id
+  count = var.env == "aat" ? 1 : 0
+}
